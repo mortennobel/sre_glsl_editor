6e4f96c73843b4dba2a19d06b4a50338e05d8345
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2018-06-04 10:56:35 +0200

    Update project. Simplify CMake structure.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "sre/SDLRenderer.hpp"
+
+using namespace sre;
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDLRenderer r;
+    r.init();
+    r.frameRender = [](){
+        RenderPass rp = RenderPass::create().build();
+        rp.drawLines({{-.5,-.5,0},{.5,.5,0},{-.5,.5,0},{.5,-.5,0}});
+    };
+    r.startEventLoop();
+
     return 0;
 }
